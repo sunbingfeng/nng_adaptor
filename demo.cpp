91fcc3a0948952f1c244a1f798fef896f14d641a
sunbingfeng <cocobill1987@gmail.com>
2023-04-27 07:56:51 +0000

    First commit

---
+++ demo.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <memory>
+
+int main(int argc, char** argv) {
+  // TODO
+  return 0;
+}
---
+++ include/nng_adaptor/nng_adaptor.h
@@ -0,0 +1,15 @@
+#ifndef NNG_ADAPTOR_H_
+#define NNG_ADAPTOR_H_
+#include <nng/nng.h>
+#include <nng/protocol/pubsub0/pub.h>
+
+namespace nng_adaptor {
+class NNGAdaptor {
+ private:
+ public:
+  NNGAdaptor() = default;
+  ~NNGAdaptor() = default;
+}
+
+}  // namespace nng_adaptor
+#endif
